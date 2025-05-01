759f42ac48fb465e540501088695c43210283dcf
Your Name <you@example.com>
2025-05-01 19:21:40 +0200

    idk

--- login-30.4/login.php
+++ login-30.4/login.php
@@ -1,15 +1,65 @@
 <?php
 
+$inputClass=["good","good","good","good"];
+
+function checkInput($data)
+{
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlspecialchars($data);
+
+    return $data;
+}
+
+function itExist($data)
+    {
+        return isset($data) && !empty($data);
+    }
+
 if ($_SERVER["REQUEST_METHOD"] === "POST")
 {
-    if(itExist($_POST["password"]) && itExist($_POST["password2"]) && itExist($_POST["mail"]))
+
+    if(itExist($_POST["userName"]) && itExist($_POST["mail"]) && itExist($_POST["subject"]) && itExist($_POST["message"]))
     {
-            return 0;
+        $name = checkInput($_POST["userName"]);
+        $mail = checkInput($_POST["mail"]);
+        $subject = checkInput($_POST["subject"]);
+        $message = checkInput($_POST["message"]);
+
+        if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+                            
+            $error[] = "Incorrect email format.";
+            $errorNum[] = 2;
+
+            
+        }
+
+        
+    }
+    else
+    {
+        $error[] = "All fields are required.";
+        if(!itExist($_POST["userName"]))
+        {
+            $inputClass[0]="error";
+        }
+        
+        if (!itExist($_POST["mail"]))
+        {
+            $inputClass[1]="error";
+        }
+        if(!itExist($_POST["subject"]))
+        {
+            $inputClass[2]="error";
+        }
+        if(!itExist($_POST["message"]))
+        {
+            $inputClass[3]="error";
+        }
     }
 
 }
->
-
+?>
 
 
 
@@ -29,22 +79,35 @@ if ($_SERVER["REQUEST_METHOD"] === "POST")
         <h1>Feedback Form</h1>
         <form action="#" method="post">
             <div id="data">
-                <div id="name">
+                <div>
                     <label for="userName">Name:</label><br>
-                    <input type="text" id="userName" name="userName" class="inputs" placeholder="Enter Name">
+                    <input type="text" id="userName" name="userName" class="inputs <?php echo $inputClass[0]; ?>" placeholder="Enter Name">
                 </div>
-                <div id="email">
+                <div >
                     <label for="mail">Email:</label><br>
-                    <input type="email" id="mail" name="mail" class="inputs" placeholder="Enter email">
+                    <input type="email" id="mail" name="mail" class="inputs <?php echo $inputClass[1]; ?>" placeholder="Enter email">
                 </div>
-                <div id="title">
+                <div>
                     <label for="subject">Subject:</label><br>
-                    <input type="text" id="subject" name="subject" class="inputs" placeholder="Enter subject">
+                    <input type="text" id="subject" name="subject" class="inputs <?php echo $inputClass[2]; ?>" placeholder="Enter subject">
                 </div>
-                <div id="text">
+                <div>
                     <label for="message">Message:</label><br>
-                    <textarea id="message" name="message" class="inputs" placeholder="Enter message" rows="7"></textarea>
+                    <textarea id="message" name="message" class="inputs <?php echo $inputClass[3]; ?>" placeholder="Enter message" rows="7"></textarea>
+
+                    <?php 
+                        if(isset($error)){
+                            for ($i=0; $i < count($error); $i++) { 
+                                echo ("<p style='color: red; font-size: 12px;'>$error[$i]</p>");
+                            }
+                            
+                        }
+
+
+                    ?>
+
                 </div>
+               
             </div>
             
             <input type="submit" value="Submit !!!">
